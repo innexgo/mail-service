bf61bd37c7d0e82e2a7ec8de827f5105541ca431
gpi <gpimpale29@gmail.com>
2024-08-26 11:56:55 -0700

    cross openssl

--- src/handlers.rs
+++ src/handlers.rs
@@ -9,75 +9,76 @@ use super::mail_service;
 use super::utils;
 
 fn report_unk_err<E: std::error::Error>(e: E) -> response::MailError {
-  utils::log(utils::Event {
-    msg: e.to_string(),
-    source: e.source().map(|e| e.to_string()),
-    severity: utils::SeverityKind::Error,
-  });
-  response::MailError::Unknown
+    utils::log(utils::Event {
+        msg: e.to_string(),
+        source: e.source().map(|e| e.to_string()),
+        severity: utils::SeverityKind::Error,
+    });
+    response::MailError::Unknown
 }
 
 pub fn fill_mail(
-  _con: &tokio_postgres::Client,
-  mail: Mail,
+    _con: &tokio_postgres::Client,
+    mail: Mail,
 ) -> Result<response::Mail, response::MailError> {
-  Ok(response::Mail {
-    mail_id: mail.mail_id,
-    request_id: mail.request_id,
-    creation_time: mail.creation_time,
-    topic: mail.topic,
-    destination: mail.destination,
-    title: mail.title,
-    content: mail.content,
-  })
+    Ok(response::Mail {
+        mail_id: mail.mail_id,
+        request_id: mail.request_id,
+        creation_time: mail.creation_time,
+        topic: mail.topic,
+        destination: mail.destination,
+        title: mail.title,
+        content: mail.content,
+    })
 }
 
 pub async fn mail_new(
-  config: Config,
-  db: Db,
-  props: request::MailNewProps,
+    config: Config,
+    db: Db,
+    props: request::MailNewProps,
 ) -> Result<response::Mail, response::MailError> {
-  let con = &mut *db.lock().await;
+    let con = &mut *db.lock().await;
 
-  if let Some(client) = config.client {
-    utils::log(utils::Event {
-      msg: format!("using AWS SESv2 to send email to {}", props.destination),
-      source: None,
-      severity: utils::SeverityKind::Info,
-    });
-    ses_aws::send_email(
-      client,
-      &config.from_address,
-      &props.destination,
-      &props.title,
-      &props.content,
-    )
-    .await;
-  } else {
-    println!("Topic: {}", &props.topic);
-    println!("To: {}", &props.destination);
-    println!("Subject: {}", &props.title);
-    println!("Body:\n {}", &props.content);
-  }
+    if let Some(client) = config.client {
+        utils::log(utils::Event {
+            msg: format!("using AWS SESv2 to send email to {}", props.destination),
+            source: None,
+            severity: utils::SeverityKind::Info,
+        });
+        ses_aws::send_email(
+            client,
+            &config.from_address,
+            &props.destination,
+            &props.title,
+            &props.content,
+        )
+        .await
+        .map_err(report_unk_err)?;
+    } else {
+        println!("Topic: {}", &props.topic);
+        println!("To: {}", &props.destination);
+        println!("Subject: {}", &props.title);
+        println!("Body:\n {}", &props.content);
+    }
 
-  let mail = mail_service::add(con, props)
-    .await
-    .map_err(report_unk_err)?;
+    let mail = mail_service::add(con, props)
+        .await
+        .map_err(report_unk_err)?;
 
-  fill_mail(con, mail)
+    fill_mail(con, mail)
 }
 
 pub async fn mail_view(
-  _config: Config,
-  db: Db,
-  props: request::MailViewProps,
+    _config: Config,
+    db: Db,
+    props: request::MailViewProps,
 ) -> Result<Vec<response::Mail>, response::MailError> {
-  let con = &mut *db.lock().await;
+    let con = &mut *db.lock().await;
 
-  // get mails
-  let mails = mail_service::query(con, props)
-    .await
-    .map_err(report_unk_err)?;
-  // return
-  mails.into_iter().map(|a| fill_mail(con, a)).collect()
+    // get mails
+    let mails = mail_service::query(con, props)
+        .await
+        .map_err(report_unk_err)?;
+    // return
+    mails.into_iter().map(|a| fill_mail(con, a)).collect()
 }
--- src/ses_aws.rs
+++ src/ses_aws.rs
@@ -1,54 +1,52 @@
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_sesv2::model::{Body, Content, Destination, EmailContent, Message};
+use aws_config::BehaviorVersion;
+use aws_sdk_sesv2::types::{Body, Content, Destination, EmailContent, Message};
 use aws_sdk_sesv2::Client;
+use aws_sdk_sesv2::Error;
 use aws_types::region::Region;
-use std::error::Error;
-
-use super::utils;
 
 pub async fn build_client() -> Client {
-  let region_provider = RegionProviderChain::default_provider().or_else(Region::new("us-west-2"));
+    let region_provider = RegionProviderChain::default_provider().or_else(Region::new("us-west-2"));
 
-  let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let shared_config = aws_config::defaults(BehaviorVersion::v2024_03_28())
+        .region(region_provider)
+        .load()
+        .await;
 
-  Client::new(&shared_config)
+    Client::new(&shared_config)
 }
 
 pub async fn send_email(
-  client: Client,
-  from_address: &str,
-  to_address: &str,
-  subject: &str,
-  html_content: &str,
-) {
-  let dest = Destination::builder().to_addresses(to_address).build();
-  let subject_content = Content::builder().data(subject).charset("UTF-8").build();
-  let body_content = Content::builder()
-    .data(html_content)
-    .charset("UTF-8")
-    .build();
-  let body = Body::builder().html(body_content).build();
+    client: Client,
+    from_address: &str,
+    to_address: &str,
+    subject: &str,
+    html_content: &str,
+) -> Result<(), Error> {
+    let dest = Destination::builder().to_addresses(to_address).build();
+    let subject_content = Content::builder().data(subject).charset("UTF-8").build()?;
 
-  let msg = Message::builder()
-    .subject(subject_content)
-    .body(body)
-    .build();
+    let body_content = Content::builder()
+        .data(html_content)
+        .charset("UTF-8")
+        .build()?;
 
-  let email_content = EmailContent::builder().simple(msg).build();
+    let body = Body::builder().html(body_content).build();
 
-  match client
-    .send_email()
-    .from_email_address(from_address)
-    .destination(dest)
-    .content(email_content)
-    .send()
-    .await
-  {
-    Ok(_) => {}
-    Err(e) => utils::log(utils::Event {
-      msg: e.to_string(),
-      source: e.source().map(|x| x.to_string()),
-      severity: utils::SeverityKind::Error,
-    }),
-  };
+    let msg = Message::builder()
+        .subject(subject_content)
+        .body(body)
+        .build();
+
+    let email_content = EmailContent::builder().simple(msg).build();
+
+    client
+        .send_email()
+        .from_email_address(from_address)
+        .destination(dest)
+        .content(email_content)
+        .send()
+        .await?;
+
+    Ok(())
 }
